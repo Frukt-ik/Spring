99a9a497300bc21cffae81bbad7fe7987e28fcc9
ArtemTasenko <wadsxz4@gmail.com>
2024-12-02 03:11:21 +0300

    small changes / comments

--- src/main/java/ua/springApp/dao/PersonDAO.java
+++ src/main/java/ua/springApp/dao/PersonDAO.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Component
 public class PersonDAO {
 
-    private JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
     @Autowired
     PersonDAO(JdbcTemplate jdbcTemplate) {
